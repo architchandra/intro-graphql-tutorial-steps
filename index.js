ab0c74b24993cc07cc3e83190646f901525e5272
Archit Chandra <architchandra@gmail.com>
2024-02-19 13:39:07 +0530

    Remove destructured parameters for clarity

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ const resolvers = {
         games() {
             return db.games;
         },
-        game(parent, { id }, context) {
-            return db.games.find((game) => game.id == id);
+        game(parent, args, context) {
+            return db.games.find((game) => game.id == args.id);
         },
         reviews() {
             return db.reviews;
         },
-        review(parent, { id }, context) {
-            return db.reviews.find((review) => review.id == id);
+        review(parent, args, context) {
+            return db.reviews.find((review) => review.id == args.id);
         },
         authors() {
             return db.authors;
         },
-        author(parent, { id }, context) {
-            return db.authors.find((author) => author.id == id);
+        author(parent, args, context) {
+            return db.authors.find((author) => author.id == args.id);
         },
     },
     Game: {
